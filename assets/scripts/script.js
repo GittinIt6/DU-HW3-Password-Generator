e99f6362f7b1d97c01c59c60c254f52661aef828
GittinIt6 <mjeluso@gmail.com>
2022-01-06 23:04:59 -0700

    major work on JS code for pop ups with logic and random generator with arrays

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,40 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let userPassword = 0
+var userPassword = "";
+var passwordLength = 8;
+let passwordLowerCase = true;
+let passwordUpperCase = true;
+let passwordNumeric = true;
+let passwordSpecial = true;
+const specChars = ["!","#","$",".","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~"];
+const alphabetLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const alphabetUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const passwordNumbers = ["0","1","2","3","4","5","6","7","8","9"];
+
 
 // Write password to the #password input
 function writePassword() {
-  // alert("Sup Dog?");
-  for (let index = 0; index < 3; index++) {
-    var passwordLength = prompt ("How many characters would you like your password to include?\n**Requirement: Minimum 8 - Maximum 128 Characters", 8);
-    if (passwordLength == null){
-      return;
-    }
-    
-    console.log(passwordLength);
-    passwordLength = Number(passwordLength);
-    console.log(typeof passwordLength);
-    console.log(passwordLength);
 
-    if ((passwordLength >= 8  && typeof passwordLength == "number")){
-      userPassword = 12345;
-      index = 2;
-    }
-    else{
-      if (index == 2){
-        confirm("!SORRY, THAT WAS YOUR LAST CHANCE!\nYou were asked many times to choose a numeric value between 8 and 128. Please try again later.");
-      }
-      else {
-        confirm("!ERROR!\nYou must choose a numeric value between 8 and 128. Please try again.");
-      }
-
-    }
-
-  }
-
-  // prompt("prompt 1", "Prompt 2");
-  // if (passwordLength)
-  // confirm("Confirm 1")
+  passwordLength = passwordLengthForm();
+  passwordComplexity();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -42,9 +24,70 @@ function writePassword() {
 
 }
 
+function passwordLengthForm() {
+  let tryCount = 2;
+  for (let index = 0; index < 3; index++) {
+    passwordLength = prompt ("How many characters would you like your password to include?\n**Requirement: Minimum 8 - Maximum 128 Characters", 8);
+    if (passwordLength == null){
+      return;
+    }
+
+    passwordLength = Number(passwordLength);
+    console.log(`password type: ${typeof passwordLength}`);
+    console.log(`password length: ${passwordLength}`);
+
+    if ((passwordLength >= 8  && passwordLength <=128 &&  typeof passwordLength == "number")){
+      // userPassword = 12345;
+      index = 2;
+    }
+    else{
+      if (index == 2){
+        alert("!SORRY, THAT WAS YOUR LAST CHANCE!\nYou were asked many times to choose a numeric value between 8 and 128. Please try again later.");
+      }
+      else {
+        alert("!ERROR!\nYou must choose a numeric value between 8 and 128. Please try again.\nYou have " + tryCount + " attempts remaining.");
+        tryCount--;
+      }
+    }
+  }
+  return passwordLength;
+}
+
+function passwordComplexity() {
+  let tryCount = 2;
+  for (let index = 0; index < 3; index++) {
+    passwordLowerCase = confirm("Your password will include lowercase characters.\nOK to continue\nCANCEL to exclude lowercase characters")
+    passwordUpperCase = confirm("Your password will include UPPERCASE characters.\nOK to continue\nCANCEL to exclude UPPERCASE characters")
+    passwordNumeric = confirm("Your password will include numbers.\nOK to continue\nCANCEL to exclude numbers")
+    passwordSpecial = confirm("Your password will include $pecial characters\nOK to continue\nCANCEL to exclude $pecial characters")
+  
+    if (passwordLowerCase == false && passwordUpperCase == false && passwordNumeric == false && passwordSpecial == false) {
+      if (index == 2) {
+        alert("Sorry, that was your last chance to make good choices.\nYou were asked multiple times to allow at least one complexity option.\nPlease try again later.");
+        tryCount--;
+      }
+      else {
+        alert("C'Mon what are you thinking?\nYou need to have characters in your password!\nAllow at least one complexity option.\nYou have " + tryCount + " attempts remaining.");
+        tryCount--;
+      }
+    }
+    else {
+      return;
+    }
+  }
+}
+
 function generatePassword() {
-  // var i = (1 + 2);
-  return userPassword;
+
+  //creating on large array of selected characters
+  const passwordItems = specChars.concat(alphabetLower,alphabetUpper,passwordNumbers);
+
+  for (let index = 0; index < passwordLength; index++) {
+    userPassword = userPassword + passwordItems[Math.floor(Math.random() * (passwordItems.length - 1))]; //user password appended by random item from passwordItems array
+  }
+  return (userPassword);
+  // return Math.floor(Math.random() * 10);
+
 }
 
 // Add event listener to generate button
