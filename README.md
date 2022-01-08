a4cd9382f54ef96d3aa35ab3be95f64af563bded
GittinIt6 <mjeluso@gmail.com>
2022-01-08 16:12:20 -0700

    final changes to functionality, updated readme, commented js code

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # DU-HW3-Password-Generator
+
+DU Coding Bootcamp Homework 3: Password Generator
+
+# Website
+
+This is a website with a form to generate a password.
+
+Public Website Address: https://gittinit6.github.io/DU-HW3-Password-Generator/
+
+## Purpose
+
+This website is specific to anyone that wants a computer generated random password based on selected criteria of:
+* Password Length: 8 - 128 characters
+* Password Complexity: uppercase, lowercase, special characters, and numbers
+
+## Audience
+
+The intended audience is an individual that would have a need for a secure and truly random password generated. The user of this site does not need to have a technical background, but *should* understand some technical terms due to the nature of a password (i.e. special characters, numeric, etc. )
+
+## Technical Detail
+
+This website uses HTML with an external linked CSS for style and external linked JavaScript code for functionality.
+
+**Responsive Design:** This website is designed to be responsive to smaller screens.
+
+The files are configured as follows:
+```
+Root Directory
+│
+│ --index.html
+│
+└───assets (folder)
+    │
+    └─── css (folder containing css style files)
+    │
+    └─── images (folder containing all image resources)
+    │
+    └─── scripts (folder containing JavaScript files)
+```
+>**CSS**: This site uses a single CSS file "style.css"
+>**JavaScript**: This site uses a single JavaScript file "script.js"
+
+The following image shows a screenshot of the website homepage:
+
+![Layout screenshot of the website showing sections](./git-images/screenshot.jpg)
+
+## Revision History 
+
+1. This website was created in January 2022 as part of a bootcamp assignment. It was created using starter code provided by the instructor.
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -12,37 +12,51 @@ const alphabetUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","
 const passwordNumbers = ["0","1","2","3","4","5","6","7","8","9"];
 
 
-// Write password to the #password input
+// Call other functions, then write password to the #password id in html
 function writePassword() {
 
+  //Get password length
   passwordLength = passwordLengthForm();
-  passwordComplexity();
+    if (passwordLength === false){
+      return;
+    }
+  //Set password complexity options
+  if (passwordComplexity() === false){
+      return;
+    }
+  //Generate password based on user input
   var password = generatePassword();
+  
+  //Write text in html for id to password
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
+//Prompt user to input password length --> Return password length as number
 function passwordLengthForm() {
-  let tryCount = 2;
+  let tryCount = 2; //variable to display number of tries left to the user
+
+  //ask user for input for password length
   for (let index = 0; index < 3; index++) {
-    passwordLength = prompt ("How many characters would you like your password to include?\n**Requirement: Minimum 8 - Maximum 128 Characters", 8);
-    if (passwordLength == null){
-      return;
+    passwordLength = prompt ("How long would you like your password?\n**Requirement: Minimum 8 - Maximum 128 Characters", 8);
+      if (passwordLength == null){
+      return false; //end process if cancel button is pressed
     }
 
-    passwordLength = Number(passwordLength);
-    console.log(`password type: ${typeof passwordLength}`);
-    console.log(`password length: ${passwordLength}`);
+    passwordLength = Number(passwordLength); //converting string output of prompt to number
+    // console.log(`password type: ${typeof passwordLength}`);
+    // console.log(`password length: ${passwordLength}`);
 
+    //checking if all parameters are met
     if ((passwordLength >= 8  && passwordLength <=128 &&  typeof passwordLength == "number")){
-      // userPassword = 12345;
       index = 2;
     }
     else{
+      //give user feedback on incorrect input and reduce number of tries remaining.
       if (index == 2){
-        alert("!SORRY, THAT WAS YOUR LAST CHANCE!\nYou were asked many times to choose a numeric value between 8 and 128. Please try again later.");
+        alert("!SORRY, THAT WAS YOUR LAST CHANCE!\nYou were asked many times to choose a numeric value between 8 - 128. Please try again later.");
+        return false;
       }
       else {
         alert("!ERROR!\nYou must choose a numeric value between 8 and 128. Please try again.\nYou have " + tryCount + " attempts remaining.");
@@ -53,18 +67,23 @@ function passwordLengthForm() {
   return passwordLength;
 }
 
+//prompt user to select various complexity options --> Sets boolean variables for future use, Returns nothing
 function passwordComplexity() {
-  let tryCount = 2;
-  for (let index = 0; index < 3; index++) {
-    passwordLowerCase = confirm("Your password will include lowercase characters.\nOK to continue\nCANCEL to exclude lowercase characters")
-    passwordUpperCase = confirm("Your password will include UPPERCASE characters.\nOK to continue\nCANCEL to exclude UPPERCASE characters")
-    passwordNumeric = confirm("Your password will include numbers.\nOK to continue\nCANCEL to exclude numbers")
-    passwordSpecial = confirm("Your password will include $pecial characters\nOK to continue\nCANCEL to exclude $pecial characters")
+  let tryCount = 2; //variable to display number of tries left to the user
   
+  //prompt for the 4 character types to be included in password
+  for (let index = 0; index < 3; index++) {
+    passwordLowerCase = confirm("Your password will include lowercase characters.\n[OK] to continue\n[CANCEL] to exclude lowercase characters")
+    passwordUpperCase = confirm("Your password will include UPPERCASE characters.\n[OK] to continue\n[CANCEL] to exclude UPPERCASE characters")
+    passwordNumeric = confirm("Your password will include numbers.\n[OK] to continue\n[CANCEL] to exclude numbers")
+    passwordSpecial = confirm("Your password will include $pecial characters\n[OK] to continue\n[CANCEL] to exclude $pecial characters")
+  
+    //check to see if at least one option was selected
     if (passwordLowerCase == false && passwordUpperCase == false && passwordNumeric == false && passwordSpecial == false) {
+      //give user feedback on incorrect input and reduce number of tries remaining. 
       if (index == 2) {
         alert("Sorry, that was your last chance to make good choices.\nYou were asked multiple times to allow at least one complexity option.\nPlease try again later.");
-        tryCount--;
+        return false;
       }
       else {
         alert("C'Mon what are you thinking?\nYou need to have characters in your password!\nAllow at least one complexity option.\nYou have " + tryCount + " attempts remaining.");
@@ -77,17 +96,31 @@ function passwordComplexity() {
   }
 }
 
+//Generates password based on criteria selected --> Returns password as string
 function generatePassword() {
-
-  //creating on large array of selected characters
-  const passwordItems = specChars.concat(alphabetLower,alphabetUpper,passwordNumbers);
-
+  var passwordItems = [];
+  userPassword = ""; //resetting userPassword
+  
+  // const passwordItems = specChars.concat(alphabetLower,alphabetUpper,passwordNumbers);
+  
+  //creating single array of selected characters based on selections
+  if (passwordLowerCase){
+    passwordItems = passwordItems.concat(alphabetLower);
+  }
+  if (passwordUpperCase){
+    passwordItems = passwordItems.concat(alphabetUpper);
+  }
+  if (passwordNumeric){
+    passwordItems = passwordItems.concat(passwordNumbers);
+  }
+  if (passwordSpecial){
+    passwordItems = passwordItems.concat(specChars);
+  }
+  //creating random password based on array contents
   for (let index = 0; index < passwordLength; index++) {
     userPassword = userPassword + passwordItems[Math.floor(Math.random() * (passwordItems.length - 1))]; //user password appended by random item from passwordItems array
   }
-  return (userPassword);
-  // return Math.floor(Math.random() * 10);
-
+  return userPassword;
 }
 
 // Add event listener to generate button
